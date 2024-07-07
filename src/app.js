f7eef20d7338009f942155aa508a1775c613989c
Lucas <medinalucasian@gmail.com>
2024-07-07 17:21:46 -0300

    changes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
-import express from 'express'
+import express from "express";
+import domainRoutes from "./routes/domainRoutes.js";
+import cors from 'express'
 
-const app= express()
+const app = express();
+const URL= 'http://localhost:4200'
+const corsMiddleware = (req, res, next) => {
+  const origin = req.headers.origin;
+  res.header("Access-Control-Allow-Headers", "Authorization, Content-Type");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Origin", URL);
+  origin === URL ? next() : res.status(403).json({ error: "Forbidden" });
+};
 
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use(cors())
+app.use(corsMiddleware)
+app.use("/domains", domainRoutes);
 
-
-app.listen(3000)
+app.listen(3000);
+console.log("Listening at port 3000");
--- src/routes/domainRoutes.js
+++ src/routes/domainRoutes.js
@@ -1,2 +1,27 @@
 import {Router} from 'express';
-import mysql from 'mysql2'
+import { pool } from '../config.js';
+const router= Router()
+
+router.get('/list',async (req,res) =>{
+    try{
+        //const[rows,fields]= await pool.execute('SELECT * from domains')
+        console.log(req.params)
+        res.send('asd')
+    }catch(err){
+        throw err
+    }
+})
+router.post('/create',async (req,res) =>{
+    try{
+        const domain= req.body
+        const userId= parseInt(domain.userId)
+        const[rows,fields]= await pool.execute(`
+            CALL createDomain(${userId}, '${domain.domainName}', '${domain.website}', ${domain.keyStatus});    
+        `)
+        res.send(true)
+    }catch(err){
+        throw err
+    }
+})
+
+export default router
