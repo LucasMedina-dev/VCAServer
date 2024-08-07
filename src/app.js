b9c47921e78720f8253df44605315d97aaeccb66
Lucas <medinalucasian@gmail.com>
2024-08-07 00:55:45 -0300

    cors fix

--- src/app.js
+++ src/app.js
@@ -1,20 +1,25 @@
 import express from "express";
 import domainRoutes from "./routes/domainRoutes.js";
 import hitRoutes from "./routes/hitRoutes.js";
-import cors from 'cors'
+import cors from 'cors';
 
 const app = express();
-const URL= 'https://visitorcounterapi.vercel.app'
+const URL = 'https://visitorcounterapi.vercel.app';
 
 app.set('trust proxy', true);
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
-app.use("/hit", hitRoutes);
-app.use(cors({
+
+app.use('/domains', cors({
   origin: URL,
   methods: 'GET,POST,PUT,DELETE',
   allowedHeaders: 'Content-Type,Authorization'
 }));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/hit", hitRoutes);
 app.use("/domains", domainRoutes);
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server running on port ${process.env.PORT || 3000}`);
+});
