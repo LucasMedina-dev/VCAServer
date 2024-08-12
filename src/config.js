d67743f9d894dde8e03fc25f8ea5f968b4800e47
Lucas <medinalucasian@gmail.com>
2024-08-12 01:50:56 -0300

    db config

--- src/config.js
+++ src/config.js
@@ -4,9 +4,9 @@ dotenv.config()
 
 
 export const pool= createPool({
-    user:'root',
-    password:'vca1234',
-    host:'127.0.0.1',
-    port: 3306,
-    database:'vcadb'
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    host:process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database:process.env.DB_DATABASE
 })
